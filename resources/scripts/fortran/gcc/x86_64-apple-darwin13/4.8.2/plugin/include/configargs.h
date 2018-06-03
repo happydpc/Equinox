d0556fac43ca0656960a488d588fd8f5e2dac2fc
muratartim <muratartim@gmail.com>
2018-06-04 01:19:57 +0200

    Fortran compiler removal.

--- resources/scripts/fortran/gcc/x86_64-apple-darwin13/4.8.2/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-4.8.2/configure --prefix=/usr/local/gfortran --with-gmp=/Users/fx/devel/gcc/deps-static/x86_64 --enable-languages=c,c++,fortran,objc,obj-c++ --build=x86_64-apple-darwin13";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "core2" } };
